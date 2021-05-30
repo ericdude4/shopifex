78617ac61f001a5037096ac5a3942d1f22982bea
ericdude4 <eric_froese2@hotmail.com>
2021-05-30 11:00:28 -0400

    mix gen migrations working

---
+++ lib/mix/tasks/gen.migration.ex
@@ -0,0 +1,58 @@
+defmodule Mix.Tasks.Shopifex.Gen.Migration do
+  @shortdoc "Generates Shopifex migration file"
+
+  @moduledoc """
+  Generates migration file.
+      mix shopifex.gen.migrations -r MyApp.Repo
+      mix shopifex.gen.migrations -r MyApp.Repo --namespace oauth2
+  This generator will add the oauth2 migration file in `priv/repo/migrations`.
+  The repository must be set under `:ecto_repos` in the current app
+  configuration or given via the `-r` option.
+  By default, the migration will be generated to the
+  "priv/YOUR_REPO/migrations" directory of the current application but it
+  can be configured to be any subdirectory of `priv` by specifying the
+  `:priv` key under the repository configuration.
+  ## Arguments
+    * `-r`, `--repo` - the repo module
+    * `--binary-id` - use binary id for primary keys
+    * `--namespace` - namespace to prepend table and schema module name
+  """
+  use Mix.Task
+
+  alias Mix.{Ecto, Shopifex, Shopifex.Migration}
+
+  @switches [binary_id: :boolean, namespace: :string]
+  @default_opts [binary_id: false, namespace: "shopify"]
+  @mix_task "shopifex.gen.migrations"
+
+  @impl true
+  def run(args) do
+    Shopifex.no_umbrella!(@mix_task)
+
+    args
+    |> Shopifex.parse_options(@switches, @default_opts)
+    |> parse()
+    |> create_migration_files(args)
+  end
+
+  defp parse({config, _parsed, _invalid}), do: config
+
+  defp create_migration_files(config, args) do
+    args
+    |> Ecto.parse_repo()
+    |> Enum.map(&ensure_repo(&1, args))
+    |> Enum.map(&Map.put(config, :repo, &1))
+    |> Enum.each(&create_migration_files/1)
+  end
+
+  defp create_migration_files(%{repo: repo, namespace: namespace} = config) do
+    name = "Create#{Macro.camelize(namespace)}Tables"
+    content = Migration.gen(name, namespace, config)
+
+    Migration.create_migration_file(repo, name, content)
+  end
+
+  defp ensure_repo(repo, args) do
+    Ecto.ensure_repo(repo, args ++ ~w(--no-deps-check))
+  end
+end
---
+++ lib/mix/tasks/shopifex.install.ex
@@ -0,0 +1,71 @@
+defmodule Mix.Tasks.Shopifex.Install do
+  @shortdoc "Installs Shopifex"
+
+  @moduledoc """
+  Generates migrations, schema module files, and updates config.
+        mix shopifex.install
+  ## Arguments
+    * `--context-app` - context app to use for path and module names
+    * `--no-migration` - don't create migration file
+    * `--no-schemas` - don't create schema module files
+  """
+
+  use Mix.Task
+
+  alias Mix.{Ecto, Shopifex}
+  alias Mix.Tasks.Shopifex.Gen.{Migration}
+  # alias Mix.Tasks.Shopifex.Gen.{Migration, Schemas}
+
+  @switches [context_app: :string, migration: :boolean, schemas: :boolean]
+  @default_opts [migration: true, schemas: true]
+  @mix_task "shopifex.install"
+
+  @impl true
+  def run(args) do
+    Shopifex.no_umbrella!(@mix_task)
+
+    args
+    |> Shopifex.parse_options(@switches, @default_opts)
+    |> parse()
+    |> IO.inspect()
+    |> run_migration(args)
+    |> run_schemas(args)
+    |> print_config_instructions(args)
+  end
+
+  defp parse({config, _parsed, _invalid}), do: config
+
+  defp run_migration(%{migration: true} = config, args) do
+    Migration.run(args)
+
+    config
+  end
+
+  defp run_migration(config, _args), do: config
+
+  defp run_schemas(%{schemas: true} = config, args) do
+    # Schemas.run(args)
+
+    config
+  end
+
+  defp run_schemas(config, _args), do: config
+
+  defp print_config_instructions(config, args) do
+    [repo | _repos] = Ecto.parse_repo(args)
+    # context_app = Map.get(config, :context_app) || Shopifex.otp_app()
+    # resource_owner = resource_owner(ProviderConfig.app_base(context_app))
+
+    # content = Config.gen(context_app, repo: inspect(repo), resource_owner: resource_owner)
+    content = ""
+
+    Mix.shell().info("""
+    Shopifex has been installed! Please append the following to `config/config.ex`:
+    #{content}
+    """)
+
+    config
+  end
+
+  defp resource_owner(base), do: inspect(Module.concat([base, "Users", "User"]))
+end
