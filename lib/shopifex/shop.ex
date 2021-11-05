40cfc492ffa25691f52706fdc203aec5d583abec
ericdude4 <eric_froese2@hotmail.com>
2021-11-05 16:31:29 -0400

    update use shop instructions

--- lib/shopifex/shop.ex
+++ lib/shopifex/shop.ex
@@ -3,10 +3,26 @@ defmodule Shopifex.Shop do
   A specification for how Shopifex is to interact with your Shopify
   shops table.
 
-  ## Options:
-      * url_field: the field which Shopifex will use to find a shop by url. Defaults to `:url`
-      * filters: a list of tuples to be used as filter keys which Shopifex will use when
-      loading your shop from your database table. Defaults to `[]`.
+  ## Options
+    * `:url_field` - the field which Shopifex will use to find a shop by url. Defaults to `:url`
+    * `:filters` - a list of tuples to be used as filter keys which Shopifex will use when
+      loading your shop from your database table. Defaults to `[]`
+
+  ## Example:
+
+  ```elixir
+  defmodule MyApp.CommercePlatformInstallation do
+    use MyApp.Schema
+    use Shopifex.Shop, url_field: :installation_identifier, filters: [{:platform, "shopify"}, {:disabled_at, nil}]
+
+    schema "commerce_platform_installations" do
+      field(:installation_identifier, :string)
+      field(:provider, :string, default: "shopify")
+      field(:disabled_at, :utc_datetime)
+      # ...
+    end
+  end
+  ```
   """
   @default_url_field :url
   @default_filter_keys []
