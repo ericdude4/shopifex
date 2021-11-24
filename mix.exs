421cf0e26404ac7953c116bc14603c818cf2b90d
ericdude4 <eric_froese2@hotmail.com>
2021-11-24 12:07:38 -0500

    inc version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Shopifex.MixProject do
   def project do
     [
       app: :shopifex,
-      version: "2.1.1",
+      version: "2.1.2",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
