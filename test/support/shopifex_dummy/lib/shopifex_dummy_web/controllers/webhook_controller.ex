6f29877c4d30403351eaae2e6bd00c0dbd65d5f9
ericdude4 <eric_froese2@hotmail.com>
2021-11-24 12:07:32 -0500

    fix warnings

--- test/support/shopifex_dummy/lib/shopifex_dummy_web/controllers/webhook_controller.ex
+++ test/support/shopifex_dummy/lib/shopifex_dummy_web/controllers/webhook_controller.ex
@@ -7,9 +7,7 @@ defmodule ShopifexDummyWeb.WebhookController do
     |> send_resp(200, "success")
   end
 
-  @doc """
-  Simply delete the shop record
-  """
+  # Simply delete the shop record
   def handle_topic(conn, shop, "app/uninstalled") do
     Shopifex.Shops.delete_shop(shop)
 
@@ -17,9 +15,7 @@ defmodule ShopifexDummyWeb.WebhookController do
     |> send_resp(200, "success")
   end
 
-  @doc """
-  Mandatory Shopify shop data erasure GDPR webhook. Simply delete the shop record
-  """
+  # Mandatory Shopify shop data erasure GDPR webhook. Simply delete the shop record
   def handle_topic(conn, shop, "shop/redact") do
     Shopifex.Shops.delete_shop(shop)
 
@@ -27,9 +23,7 @@ defmodule ShopifexDummyWeb.WebhookController do
     |> send_resp(204, "")
   end
 
-  @doc """
-  Mandatory Shopify customer data erasure GDPR webhook. Simply delete the shop (customer) record
-  """
+  # Mandatory Shopify customer data erasure GDPR webhook. Simply delete the shop (customer) record
   def handle_topic(conn, shop, "customers/redact") do
     Shopifex.Shops.delete_shop(shop)
 
@@ -37,9 +31,7 @@ defmodule ShopifexDummyWeb.WebhookController do
     |> send_resp(204, "")
   end
 
-  @doc """
-  Mandatory Shopify customer data request GDPR webhook.
-  """
+  # Mandatory Shopify customer data request GDPR webhook.
   def handle_topic(conn, _shop, "customers/data_request") do
     # Send an email of the shop data to the customer.
     conn
