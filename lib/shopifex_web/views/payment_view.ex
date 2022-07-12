113fe9aa0db533d6508badd06fe0ef6303109ac0
ericdude4 <eric_froese2@hotmail.com>
2022-07-12 10:36:10 -0400

    fix compile/runtime config issue

--- lib/shopifex_web/views/payment_view.ex
+++ lib/shopifex_web/views/payment_view.ex
@@ -1,11 +1,9 @@
 defmodule ShopifexWeb.PaymentView do
   use ShopifexWeb, :view
 
-  @payment_guard Application.compile_env(:shopifex, :payment_guard)
-
   def available_plans(shop, guard) do
     shop
-    |> @payment_guard.list_available_plans_for_guard(guard)
+    |> payment_guard().list_available_plans_for_guard(guard)
     |> Enum.map(fn guard ->
       Map.take(guard, [:id, :features, :grants, :name, :price, :type, :usages])
     end)
@@ -17,4 +15,6 @@ defmodule ShopifexWeb.PaymentView do
     |> Shopifex.Plug.current_shop()
     |> Shopifex.Shops.get_url()
   end
+
+  def payment_guard, do: Application.get_env(:shopifex, :payment_guard)
 end
