c20b92641883a119aea7000c690bc9af46a772df
ericdude4 <eric_froese2@hotmail.com>
2024-12-09 15:19:57 -0500

    inc bugfix version w/ handle unauthorized json requests with 403 response instead of HTTP redirect

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Shopifex.MixProject do
   def project do
     [
       app: :shopifex,
-      version: "2.2.1",
+      version: "2.2.2",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       compilers: Mix.compilers(),
