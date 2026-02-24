941c535cca30e7e7665867e8b1a0d860b48db50e
ericdude4 <eric_froese2@hotmail.com>
2026-02-24 15:23:50 -0500

    increase dependancy versions

--- test/support/shopifex_dummy/lib/shopifex_dummy_web/gettext.ex
+++ test/support/shopifex_dummy/lib/shopifex_dummy_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule ShopifexDummyWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :shopifex
+  use Gettext.Backend, otp_app: :shopifex
 end
